53ee67eb041195e548ef08c1f6f698d0b0f48141
Agastya Chandrakant <me@hanabi.in>
2020-05-17 22:02:56 +0530

    header update

---
+++ src/comp/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="mb-5">
+      <div className="page-header mt-3">
+        <h1>time convertor</h1>
+        <hr />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
