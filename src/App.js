ac2f6c30abfddfc5befb0fdf5c4cab6a10093b7c
Agastya Chandrakant <me@hanabi.in>
2020-05-17 22:12:40 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from './comp/header';
+import Footer from './comp/footer';
 import CurrentTime from './comp/currentTime';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="container-fluid">
       <Header />
       <CurrentTime />
+      <Footer />
     </div>
   );
 }
---
+++ src/comp/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="mt-5 w-100" style={{ bottom: 0, position: 'absolute' }}>
+      <hr />
+      <div className="page-footer text-center">
+        <h5>
+          Copyright &copy; {year}{' '}
+          <a href="https://en.wikinews.org/wiki/User:Acagastya">acagastya</a>.
+          This project is licensed under{' '}
+          <a href="https://opensource.org/licenses/BSD-3-Clause">
+            BSD-3-Clause license
+          </a>
+          .
+        </h5>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
