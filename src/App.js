d9bde53c9b4da2079ac755a4b93e53cb0f156245
Agastya Chandrakant <me@hanabi.in>
2020-05-17 22:03:20 +0530

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import Header from './comp/header';
+import CurrentTime from './comp/currentTime';
 
 function App() {
-  return <div className="container-fluid">Hello world</div>;
+  return (
+    <div className="container-fluid">
+      <Header />
+      <CurrentTime />
+    </div>
+  );
 }
 
 export default App;
