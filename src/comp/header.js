9f2026dbecb6e173c182bc887db97761ec48a701
Agastya Chandrakant <me@hanabi.in>
2020-05-18 11:59:29 +0530

    rmv unused code

--- src/comp/header.js
+++ src/comp/header.js
@@ -43,32 +43,4 @@ function Header() {
   );
 }
 
-function Header1() {
-  return (
-    <div className="dropdown">
-      <button
-        className="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-      >
-        Dropdown button
-      </button>
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a className="dropdown-item" href="#">
-          Action
-        </a>
-        <a className="dropdown-item" href="#">
-          Another action
-        </a>
-        <a className="dropdown-item" href="#">
-          Something else here
-        </a>
-      </div>
-    </div>
-  );
-}
-
 export default Header;
