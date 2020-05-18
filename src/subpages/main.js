e09fa3cd13a600d47456c616fd1e4618eeb3d47d
Agastya Chandrakant <me@hanabi.in>
2020-05-18 07:28:33 +0530

    add class

--- src/subpages/main.js
+++ src/subpages/main.js
@@ -6,7 +6,10 @@ import Header from '../comp/header';
 
 function Main() {
   return (
-    <div className="container-fluid">
+    <div
+      className="container-fluid"
+      style={{ maxWidth: '100vw', overflow: 'hidden', padding: 0 }}
+    >
       <Header />
       <CurrentTime />
       <Footer />
