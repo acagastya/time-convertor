974da7d8c2a5ba90ec9cf20ab4dfd344c2010ac0
Agastya Chandrakant <me@hanabi.in>
2020-05-18 07:47:17 +0530

    add help

--- src/subpages/help.js
+++ src/subpages/help.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment-timezone';
+
 import Footer from '../comp/footer';
 import Header from '../comp/header';
 
 function Help() {
+  const tz = moment.tz.guess();
+  const tzAbbr = moment.tz.zone(tz).abbr(moment());
   return (
-    <div className="container-fluid">
+    <div
+      className="container-fluid"
+      style={{ maxWidth: '100vw', overflow: 'hidden', padding: 0 }}
+    >
       <Header />
-      This is help
+      <ol>
+        <li>
+          Use the <Link to="/">homepage</Link> to convert your{' '}
+          <span className="font-weight-bold">current time</span> ({tzAbbr}) to
+          other timezone.
+        </li>
+        <li>
+          Use the <Link to="/future">/future</Link> to convert a{' '}
+          <span className="font-weight-bold">future date</span> between
+          different timezones.
+        </li>
+        <li>
+          Use the <Link to="/from-to">/from-to</Link> to convert
+          <span className="font-weight-bold">current time</span> between
+          different timezones.
+        </li>
+      </ol>
       <Footer />
     </div>
   );
