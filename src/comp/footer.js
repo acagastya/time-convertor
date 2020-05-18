038537356509f216acac897e2781ce4f048bf7d1
Agastya Chandrakant <me@hanabi.in>
2020-05-18 07:20:24 +0530

    smaller size

--- src/comp/footer.js
+++ src/comp/footer.js
@@ -3,28 +3,29 @@ import React from 'react';
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="mt-5 w-100" style={{ bottom: 0, position: 'absolute' }}>
+    <footer
+      className="mt-5 w-100"
+      style={{ bottom: 0, left: 0, position: 'fixed' }}
+    >
       <hr />
-      <div className="page-footer text-center">
-        <h5 aria-level="5">
-          Copyright &copy; {year}{' '}
-          <a
-            href="https://en.wikinews.org/wiki/User:Acagastya"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            acagastya
-          </a>
-          . This project is licensed under{' '}
-          <a
-            href="https://opensource.org/licenses/BSD-3-Clause"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BSD-3-Clause license
-          </a>
-          .
-        </h5>
+      <div className="page-footer text-center" style={{ padding: '5px' }}>
+        Copyright &copy; {year}{' '}
+        <a
+          href="https://en.wikinews.org/wiki/User:Acagastya"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          acagastya
+        </a>
+        . This project is licensed under{' '}
+        <a
+          href="https://opensource.org/licenses/BSD-3-Clause"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          BSD-3-Clause license
+        </a>
+        .
       </div>
     </footer>
   );
